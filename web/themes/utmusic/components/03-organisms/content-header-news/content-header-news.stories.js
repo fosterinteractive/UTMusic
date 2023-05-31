f9ddd49f27d28ee1e184028a5bc856b8c427ed86
Kate Karpenko <kate@fosterinteractive.com>
2023-05-31 09:18:47 -0600

    update content-header for News in SB & Drupal

---
+++ web/themes/utmusic/components/03-organisms/content-header-news/content-header-news.stories.js
@@ -0,0 +1,30 @@
+import heroNews from './content-header-news.twig';
+
+import contentHeaderLandscapeData from './content-header-news--landscape.yml';
+import contentHeaderPortraitData from './content-header-news--portrait.yml';
+import contentHeaderVideoData from './content-header-news--video.yml';
+import contentHeaderTextData from './content-header-news--text.yml';
+
+/**
+ * Storybook Definition with data documentation and controls
+ */
+export default {
+  title: 'Organisms/Content Header News',
+  component: heroNews,
+
+  parameters: {
+    controls: { hideNoControlsWarning: 'true' },
+    docs: {
+      description: {
+        component: 'Organism Content Header. Content Header Banners appear at the top of advanced pages',
+      },
+    },
+  },
+}
+
+export const ContentHeaderNewsImageLandscape = () => heroNews(contentHeaderLandscapeData);
+export const ContentHeaderNewsImagePortrait = () => heroNews(contentHeaderPortraitData);
+export const ContentHeaderNewsVideo = () => heroNews(contentHeaderVideoData);
+
+export const ContentHeaderNewsText = () => heroNews(contentHeaderTextData);
+
