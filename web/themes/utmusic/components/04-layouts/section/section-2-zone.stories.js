fcb9e6da80bd18f3bf286326cc28f3b36c3ca7ae
Kate Karpenko <kate@fosterinteractive.com>
2023-06-22 16:02:25 -0600

    update layouts in SB

--- web/themes/utmusic/components/04-layouts/section/section-2-zone.stories.js
+++ web/themes/utmusic/components/04-layouts/section/section-2-zone.stories.js
@@ -1,12 +1,22 @@
 // Buttons Stories
 import section from './section.twig';
 import section2ZoneData from './section--[6|6].yml';
-import section2Zone2col55Data from './section--[5|5].yml';
-import section2Zone2col44Data from './section--[4|4].yml';
-import section2Zone2col75Data from './section--[7|5].yml';
-import section2Zone2col57Data from './section--[5|7].yml';
+
+import section2Zone2col38Data from './section--[3|8].yml';
+import section2Zone2col39Data from './section--[3|9].yml';
+import section2Zone2col46Data from './section--[4|6].yml';
+import section2Zone2col47Data from './section--[4|7].yml';
 import section2Zone2col48Data from './section--[4|8].yml';
+import section2Zone2col55Data from './section--[5|5].yml';
+import section2Zone2col56Data from './section--[5|6].yml';
+import section2Zone2col57Data from './section--[5|7].yml';
+import section2Zone2col64Data from './section--[6|4].yml';
+import section2Zone2col65Data from './section--[6|5].yml';
+import section2Zone2col74Data from './section--[7|4].yml';
+import section2Zone2col75Data from './section--[7|5].yml';
+import section2Zone2col83Data from './section--[8|3].yml';
 import section2Zone2col84Data from './section--[8|4].yml';
+import section2Zone2col93Data from './section--[9|3].yml';
 
 /**
  * Storybook Definition with data documentation and controls
@@ -34,13 +44,22 @@ export default {
       control: {
         type: 'select',
         options: { 
-          '[4|4]': '[4|4]',
+          '[3|8]': '[3|8]',
+          '[3|9]': '[3|9]',
+          '[4|6]': '[4|6]',
+          '[4|7]': '[4|7]',
+          '[4|8]': '[4|8]',
           '[5|5]': '[5|5]',
-          '[6|6]': '[6|6]',
-          '[7|5]': '[7|5]',
+          '[5|6]': '[5|6]',
           '[5|7]': '[5|7]', 
+          '[6|4]': '[6|4]',
+          '[6|5]': '[6|5]',
+          '[6|6]': '[6|6]',
+          '[7|4]': '[7|4]',
+          '[7|5]': '[7|5]',
+          '[8|3]': '[8|3]', 
           '[8|4]': '[8|4]', 
-          '[4|8]': '[4|8]', 
+          '[9|3]': '[9|3]', 
         },
       },
     },
@@ -69,11 +88,10 @@ export default {
         options: { 
           'Default (White Background)': '', 
           'Black Background': 'black-bg', 
-          'Light Yellow Background': 'light-yellow-bg', 
         },
       },
       table: {
-        type: { summary: 'null | black-bg | light-yellow-bg' },
+        type: { summary: 'null | black-bg' },
       },
     },
 
@@ -164,25 +182,49 @@ section({
   });
 
 
-  Section2Zone.storyName = '2 cols: [6|6] wide';
+Section2Zone.storyName = '2 cols: [6|6] wide';
 
+export const section2Zone2col38 = () => section(section2Zone2col38Data);
+section2Zone2col38.storyName = '2 cols: [3|8] wide';
 
+export const section2Zone2col39 = () => section(section2Zone2col39Data);
+section2Zone2col39.storyName = '2 cols: [3|9] wide';
 
-export const section2Zone2col55 = () => section(section2Zone2col55Data);
-section2Zone2col55.storyName = '2 cols: [5|5] wide';
+export const section2Zone2col46 = () => section(section2Zone2col46Data);
+section2Zone2col46.storyName = '2 cols: [4|6] wide';
 
-export const section2Zone2col44 = () => section(section2Zone2col44Data);
-section2Zone2col44.storyName = '2 cols: [4|4] wide';
-
-export const section2Zone2col75 = () => section(section2Zone2col75Data);
-section2Zone2col75.storyName = '2 cols: [7|5] wide';
-
-export const section2Zone2col57 = () => section(section2Zone2col57Data);
-section2Zone2col57.storyName = '2 cols: [5|7] wide';
+export const section2Zone2col47 = () => section(section2Zone2col47Data);
+section2Zone2col47.storyName = '2 cols: [4|7] wide';
 
 export const section2Zone2col48 = () => section(section2Zone2col48Data);
 section2Zone2col48.storyName = '2 cols: [4|8] wide';
 
+export const section2Zone2col55 = () => section(section2Zone2col55Data);
+section2Zone2col55.storyName = '2 cols: [5|5] wide';
+
+export const section2Zone2col56 = () => section(section2Zone2col56Data);
+section2Zone2col56.storyName = '2 cols: [5|6] wide';
+
+export const section2Zone2col57 = () => section(section2Zone2col57Data);
+section2Zone2col57.storyName = '2 cols: [5|7] wide';
+
+export const section2Zone2col64 = () => section(section2Zone2col64Data);
+section2Zone2col64.storyName = '2 cols: [6|4] wide';
+
+export const section2Zone2col65 = () => section(section2Zone2col65Data);
+section2Zone2col65.storyName = '2 cols: [6|5] wide';
+
+export const section2Zone2col74 = () => section(section2Zone2col74Data);
+section2Zone2col74.storyName = '2 cols: [7|4] wide';
+
+export const section2Zone2col75 = () => section(section2Zone2col75Data);
+section2Zone2col75.storyName = '2 cols: [7|5] wide';
+
+export const section2Zone2col83 = () => section(section2Zone2col83Data);
+section2Zone2col83.storyName = '2 cols: [8|3] wide';
+
 export const section2Zone2col84 = () => section(section2Zone2col84Data);
 section2Zone2col84.storyName = '2 cols: [8|4] wide';
 
+export const section2Zone2col93 = () => section(section2Zone2col93Data);
+section2Zone2col93.storyName = '2 cols: [9|3] wide';
