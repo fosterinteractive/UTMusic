c260315b20fe713a4d73cfbeced85c21de4f13ea
Kate Karpenko <kate@fosterinteractive.com>
2023-07-06 20:19:26 -0600

    hide arrows and scroll if only 2 video, update JS for featured media

--- web/themes/utmusic/components/02-molecules/featured-media/featured-media.js
+++ web/themes/utmusic/components/02-molecules/featured-media/featured-media.js
@@ -1,95 +1,99 @@
-function currentIndex(carouselContainer, itemWidth) {
-  let currentIndex;
-  if (window.innerWidth < 400) {
-    currentIndex = Math.round(carouselContainer.scrollLeft / itemWidth);
-  } else {
-    if (carouselContainer.scrollLeft === 0) {
-      currentIndex = Math.round((carouselContainer.scrollLeft + itemWidth / 3 ) / itemWidth);
+(function ($) {
+  'use strict';
+
+  function currentIndex(carouselContainer, itemWidth) {
+    let currentIndex;
+    if (window.innerWidth < 400) {
+      currentIndex = Math.round(carouselContainer.scrollLeft / itemWidth);
     } else {
-      currentIndex = Math.round((carouselContainer.scrollLeft + itemWidth / 2 ) / itemWidth);
+      if (carouselContainer.scrollLeft === 0) {
+        currentIndex = Math.round((carouselContainer.scrollLeft + itemWidth / 3 ) / itemWidth);
+      } else {
+        currentIndex = Math.round((carouselContainer.scrollLeft + itemWidth / 2 ) / itemWidth);
+      }
     }
+
+    return currentIndex;
   }
 
-  return currentIndex;
-}
-
-Drupal.behaviors.featuredMediaDrupal = {
-  attach(context) {
-    // Find carousel container with controls buttons and items
-    const carouselContainer = document.querySelector('.m-featured-media__items');
-    if (!carouselContainer) {
-      return;
-    }
-    
-    const allItems = carouselContainer.querySelectorAll('.m-featured-media__item');
-
-    const nextButton = context.querySelector('.m-featured-media__items-next-btn');
-    const prevButton = context.querySelector('.m-featured-media__items-prev-btn');
-
-    //On loading page we hide previous button
-    prevButton.style.visibility = 'hidden';
-
-    // Create new region (if it doesn't not exist for screenreaders)
-    const liveregionExist = context.querySelector('.liveregion');
-    if (!liveregionExist) {
-      var liveregion = document.createElement('div');
-      liveregion.setAttribute('aria-live', 'polite');
-      liveregion.setAttribute('aria-atomic', 'true');
-      liveregion.setAttribute('class', 'liveregion u-visually--hidden visually-hidden');
-      carouselContainer.closest('.m-featured-media__items-wrapper').appendChild(liveregion);
-    } 
-    
-    nextButton.addEventListener('click', (e)=> {
-      e.preventDefault();
-      const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
-      const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
-      // this line scrolls the items
-      carousel.scrollLeft += itemWidth;
-      //visibility previous button
-      prevButton.style.visibility = 'visible';
-
-      let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
-      // Message for screen-reader in the visually-hidden region
-      e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = 'Item ' + currentItem + ' of ' + allItems.length;
-
-      const isEndOfScroll = carousel.scrollWidth - carousel.scrollLeft === carousel.clientWidth;
-      nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
-    });
-
-    prevButton.addEventListener('click', (e)=> {
-      e.preventDefault();
-
-      const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
-      const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
-      // this line scrolls the items
-      carousel.scrollLeft -= itemWidth;
-      //visibility next button
-      nextButton.style.visibility = 'visible';
-
-      let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
-      // Message for screen-reader in the visually-hidden region
-      e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = 'Item ' + currentItem + ' of ' + allItems.length;
-
-      const isStartOfScroll = carousel.scrollLeft === 0;
-      prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
-    });
+  Drupal.behaviors.featuredMediaDrupal = {
+    attach(context) {
+      // Find carousel container with controls buttons and items
+      const carouselContainers = context.querySelectorAll('.m-featured-media__items');
+      if (!carouselContainers) {
+        return;
+      }
 
 
+      carouselContainers.forEach(carouselContainer => {
+        const allItems = carouselContainer.querySelectorAll('.m-featured-media__item');
 
-    carouselContainer.addEventListener('scroll', (e)=> {
-      const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
-      const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+        const nextButton = carouselContainer.closest('.m-featured-media__items-wrapper').querySelector('.m-featured-media__items-next-btn');
+        const prevButton = carouselContainer.closest('.m-featured-media__items-wrapper').querySelector('.m-featured-media__items-prev-btn');
+  
+        //On loading page we hide previous button
+        prevButton.style.visibility = 'hidden';
+        
+        // Create new region (if it doesn't not exist for screenreaders)
+        const liveregionExist = carouselContainer.closest('.m-featured-media__items-wrapper').querySelector('.liveregion');
+        if (!liveregionExist) {
+          var liveregion = document.createElement('div');
+          liveregion.setAttribute('aria-live', 'polite');
+          liveregion.setAttribute('aria-atomic', 'true');
+          liveregion.setAttribute('class', 'liveregion u-visually--hidden visually-hidden');
+          carouselContainer.closest('.m-featured-media__items-wrapper').appendChild(liveregion);
+        }
 
-      let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
-      // Message for screen-reader in the visually-hidden region
-      e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = 'Item ' + currentItem + ' of ' + allItems.length;
-      
-      const isEndOfScroll = carouselContainer.scrollWidth - carouselContainer.scrollLeft === carouselContainer.clientWidth;
-      nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
-
-      const isStartOfScroll = carouselContainer.scrollLeft === 0;
-      prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
-    });
-
-  },
-};
+        nextButton.addEventListener('click', (e)=> {
+          e.preventDefault();
+          const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
+          const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+          // this line scrolls the items
+          carousel.scrollLeft += itemWidth;
+          //visibility previous button
+          prevButton.style.visibility = 'visible';
+  
+          let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
+          // Message for screen-reader in the visually-hidden region
+          e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = Drupal.t('Item ' + currentItem + ' of ' + allItems.length);
+  
+          const isEndOfScroll = carousel.scrollWidth - carousel.scrollLeft === carousel.clientWidth;
+          nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
+        });
+  
+        prevButton.addEventListener('click', (e)=> {
+          e.preventDefault();
+  
+          const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
+          const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+          // this line scrolls the items
+          carousel.scrollLeft -= itemWidth;
+          //visibility next button
+          nextButton.style.visibility = 'visible';
+  
+          let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
+          // Message for screen-reader in the visually-hidden region
+          e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = Drupal.t('Item ' + currentItem + ' of ' + allItems.length);
+  
+          const isStartOfScroll = carousel.scrollLeft === 0;
+          prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
+        });
+  
+        carouselContainer.addEventListener('scroll', (e)=> {
+          const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
+          const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+  
+          let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
+          // Message for screen-reader in the visually-hidden region
+          e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = Drupal.t('Item ' + currentItem + ' of ' + allItems.length);
+          
+          const isEndOfScroll = carouselContainer.scrollWidth - carouselContainer.scrollLeft === carouselContainer.clientWidth;
+          nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
+  
+          const isStartOfScroll = carouselContainer.scrollLeft === 0;
+          prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
+        });
+      });
+    },
+  };
+})(jQuery);
--- web/themes/utmusic/js/featured-media.js
+++ web/themes/utmusic/js/featured-media.js
@@ -19,82 +19,85 @@
   Drupal.behaviors.featuredMediaDrupal = {
     attach(context) {
       // Find carousel container with controls buttons and items
-      const carouselContainer = document.querySelector('.m-featured-media__items');
-      if (!carouselContainer) {
+      const carouselContainers = context.querySelectorAll('.m-featured-media__items');
+      if (!carouselContainers) {
         return;
       }
-      const allItems = carouselContainer.querySelectorAll('.m-featured-media__item');
-
-      const nextButton = context.querySelector('.m-featured-media__items-next-btn');
-      const prevButton = context.querySelector('.m-featured-media__items-prev-btn');
-
-      //On loading page we hide previous button
-      prevButton.style.visibility = 'hidden';
-
-      // Create new region (if it doesn't not exist for screenreaders)
-      const liveregionExist = context.querySelector('.liveregion');
-      if (!liveregionExist) {
-        var liveregion = document.createElement('div');
-        liveregion.setAttribute('aria-live', 'polite');
-        liveregion.setAttribute('aria-atomic', 'true');
-        liveregion.setAttribute('class', 'liveregion u-visually--hidden visually-hidden');
-        carouselContainer.closest('.m-featured-media__items-wrapper').appendChild(liveregion);
-      } 
-      
-      nextButton.addEventListener('click', (e)=> {
-        e.preventDefault();
-        const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
-        // const allItems = carousel.querySelectorAll('.m-featured-media__item');
-        const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
-        // this line scrolls the items
-        carousel.scrollLeft += itemWidth;
-        //visibility previous button
-        prevButton.style.visibility = 'visible';
-
-        let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
-        // Message for screen-reader in the visually-hidden region
-        e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = Drupal.t('Item ' + currentItem + ' of ' + allItems.length);
-
-        const isEndOfScroll = carousel.scrollWidth - carousel.scrollLeft === carousel.clientWidth;
-        nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
-      });
-
-      prevButton.addEventListener('click', (e)=> {
-        e.preventDefault();
-
-        const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
-        // const allItems = carousel.querySelectorAll('.m-featured-media__item');
-        const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
-        // this line scrolls the items
-        carousel.scrollLeft -= itemWidth;
-        //visibility next button
-        nextButton.style.visibility = 'visible';
-
-        let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
-        // Message for screen-reader in the visually-hidden region
-        e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = Drupal.t('Item ' + currentItem + ' of ' + allItems.length);
-
-        const isStartOfScroll = carousel.scrollLeft === 0;
-        prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
-      });
 
 
+      carouselContainers.forEach(carouselContainer => {
+        const allItems = carouselContainer.querySelectorAll('.m-featured-media__item');
 
-      carouselContainer.addEventListener('scroll', (e)=> {
-        const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
-        const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+        const nextButton = carouselContainer.closest('.m-featured-media__items-wrapper').querySelector('.m-featured-media__items-next-btn');
+        const prevButton = carouselContainer.closest('.m-featured-media__items-wrapper').querySelector('.m-featured-media__items-prev-btn');
+  
+        if ((!prevButton)||(!nextButton)) {
+          return;
+        }
 
-        let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
-        // Message for screen-reader in the visually-hidden region
-        e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = Drupal.t('Item ' + currentItem + ' of ' + allItems.length);
+        //On loading page we hide previous button
+        prevButton.style.visibility = 'hidden';
         
-        const isEndOfScroll = carouselContainer.scrollWidth - carouselContainer.scrollLeft === carouselContainer.clientWidth;
-        nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
+        // Create new region (if it doesn't not exist for screenreaders)
+        const liveregionExist = carouselContainer.closest('.m-featured-media__items-wrapper').querySelector('.liveregion');
+        if (!liveregionExist) {
+          var liveregion = document.createElement('div');
+          liveregion.setAttribute('aria-live', 'polite');
+          liveregion.setAttribute('aria-atomic', 'true');
+          liveregion.setAttribute('class', 'liveregion u-visually--hidden visually-hidden');
+          carouselContainer.closest('.m-featured-media__items-wrapper').appendChild(liveregion);
+        }
 
-        const isStartOfScroll = carouselContainer.scrollLeft === 0;
-        prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
+        nextButton.addEventListener('click', (e)=> {
+          e.preventDefault();
+          const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
+          const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+          // this line scrolls the items
+          carousel.scrollLeft += itemWidth;
+          //visibility previous button
+          prevButton.style.visibility = 'visible';
+  
+          let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
+          // Message for screen-reader in the visually-hidden region
+          e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = Drupal.t('Item ' + currentItem + ' of ' + allItems.length);
+  
+          const isEndOfScroll = carousel.scrollWidth - carousel.scrollLeft === carousel.clientWidth;
+          nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
+        });
+  
+        prevButton.addEventListener('click', (e)=> {
+          e.preventDefault();
+  
+          const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
+          const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+          // this line scrolls the items
+          carousel.scrollLeft -= itemWidth;
+          //visibility next button
+          nextButton.style.visibility = 'visible';
+  
+          let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
+          // Message for screen-reader in the visually-hidden region
+          e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = Drupal.t('Item ' + currentItem + ' of ' + allItems.length);
+  
+          const isStartOfScroll = carousel.scrollLeft === 0;
+          prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
+        });
+  
+        carouselContainer.addEventListener('scroll', (e)=> {
+          const carousel = e.target.closest('.m-featured-media').querySelector('.m-featured-media__items');
+          const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+  
+          let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
+          // Message for screen-reader in the visually-hidden region
+          e.target.closest('.m-featured-media').querySelector('.liveregion').innerText = Drupal.t('Item ' + currentItem + ' of ' + allItems.length);
+          
+          const isEndOfScroll = carouselContainer.scrollWidth - carouselContainer.scrollLeft === carouselContainer.clientWidth;
+          nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
+  
+          const isStartOfScroll = carouselContainer.scrollLeft === 0;
+          prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
+        });
       });
-
     },
   };
 })(jQuery);
