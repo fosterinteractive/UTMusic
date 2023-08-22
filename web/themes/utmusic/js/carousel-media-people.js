19320035f7dcc333991c1b7c35e898187dcbd2bb
Kate Karpenko <kate@fosterinteractive.com>
2023-08-21 18:44:47 -0600

    create view for people related to programs, update JS (if items less then viewport width)

--- web/themes/utmusic/js/carousel-media-people.js
+++ web/themes/utmusic/js/carousel-media-people.js
@@ -13,6 +13,50 @@ function currentIndex(carouselContainer, itemWidth) {
   return currentIndex;
 }
 
+const hideControlButtons = (carouselContainer, nextButton, prevButton) => {
+  // check how many items and if total sum of their width is less than viewport - then we hide control buttons and scrollbar
+  let total_items = carouselContainer.querySelectorAll('.o-listing--people-carousel__item');
+  let total_width = 0;
+  for (let i = 0; i < total_items.length; i++) {
+
+    total_width += total_items[i].clientWidth;
+  }
+
+  total_width = total_width + 30 * (total_items.length - 1);
+
+  console.log(total_width)
+
+  if (carouselContainer.clientWidth > total_width) {
+    console.log('test')
+
+    if (prevButton) {
+      //On loading page we hide previous button
+      prevButton.style.visibility = 'hidden';
+    }
+
+    if (nextButton) {
+      //On loading page we hide previous button
+      nextButton.style.visibility = 'hidden';
+    }
+
+    carouselContainer.style.overflowX = 'unset';
+
+  } else {
+    if (prevButton) {
+      //On loading page we hide previous button
+      prevButton.style.visibility = 'visible';
+    }
+
+    if (nextButton) {
+      //On loading page we hide previous button
+      nextButton.style.visibility = 'visible';
+    }
+
+    carouselContainer.style.overflowX = 'scroll';
+
+  }
+}
+
 Drupal.behaviors.listingPeopleCarousel = {
   attach(context) {
     // Find carousel container with controls buttons and items
@@ -28,22 +72,15 @@ Drupal.behaviors.listingPeopleCarousel = {
       return;
     }
 
-    let firstImageHeight = context.querySelector('.m-listing-item--people .m-listing-item--people__img img').height;
+    let firstImageHeight = context.querySelector('.m-listing-item--people .m-listing-item--people__img img')?.height;
     let placeholdersArray = context.querySelectorAll('.m-listing-item--people .m-listing-item--people__img-illustration');
 
     placeholdersArray.forEach( placeholder => {
-      placeholder.style.height = firstImageHeight ? firstImageHeight + "px": '300px';
+      placeholder.style.height = firstImageHeight ? firstImageHeight + "px" : '300px';
     });
 
     const allItems = carouselContainer.querySelectorAll('.m-listing-item--people-carousel');
     
-
-    const nextButton = context.querySelector('.o-listing--people-carousel__items-next-btn');
-    const prevButton = context.querySelector('.o-listing--people-carousel__items-prev-btn');
-
-    //On loading page we hide previous button
-    prevButton.style.visibility = 'hidden';
-
     // Create new region (if it doesn't not exist for screenreaders)
     const liveregionExist = carouselContainer.closest('.o-listing--people-carousel').querySelector('.liveregion');
 
@@ -55,40 +92,51 @@ Drupal.behaviors.listingPeopleCarousel = {
       carouselContainer.closest('.o-listing--people-carousel').appendChild(liveregion);
     } 
 
-    nextButton.addEventListener('click', (e)=> {
-      e.preventDefault();
-      const carousel = e.target.closest('.o-listing--people-carousel').querySelector('.o-listing--people-carousel__items');
-      const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
-      // this line scrolls the items
-      carousel.scrollLeft += itemWidth;
-      //visibility previous button
-      prevButton.style.visibility = 'visible';
+    let nextButton = context.querySelector('.o-listing--people-carousel__items-next-btn');
+    
+    if (nextButton) {
+      nextButton.addEventListener('click', (e)=> {
+        e.preventDefault();
+        const carousel = e.target.closest('.o-listing--people-carousel').querySelector('.o-listing--people-carousel__items');
+        const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+        // this line scrolls the items
+        carousel.scrollLeft += itemWidth;
+        //visibility previous button
+        prevButton.style.visibility = 'visible';
+  
+        let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
+        // Message for screen-reader in the visually-hidden region
+        e.target.closest('.o-listing--people-carousel').querySelector('.liveregion').innerText = ('Item ' + currentItem + ' of ' + allItems.length);
+  
+        const isEndOfScroll = carousel.scrollWidth - carousel.scrollLeft === carousel.clientWidth;
+        nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
+      });
+    }
 
-      let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
-      // Message for screen-reader in the visually-hidden region
-      e.target.closest('.o-listing--people-carousel').querySelector('.liveregion').innerText = ('Item ' + currentItem + ' of ' + allItems.length);
+    let prevButton = context.querySelector('.o-listing--people-carousel__items-prev-btn');
 
-      const isEndOfScroll = carousel.scrollWidth - carousel.scrollLeft === carousel.clientWidth;
-      nextButton.style.visibility = isEndOfScroll ? 'hidden' : 'visible';
-    });
-
-    prevButton.addEventListener('click', (e)=> {
-      e.preventDefault();
-
-      const carousel = e.target.closest('.o-listing--people-carousel').querySelector('.o-listing--people-carousel__items');
-      const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
-      // this line scrolls the items
-      carousel.scrollLeft -= itemWidth;
-      //visibility next button
-      nextButton.style.visibility = 'visible';
-
-      let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
-      // Message for screen-reader in the visually-hidden region
-      e.target.closest('.o-listing--people-carousel').querySelector('.liveregion').innerText = ('Item ' + currentItem + ' of ' + allItems.length);
-
-      const isStartOfScroll = carousel.scrollLeft === 0;
-      prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
-    });
+    if (prevButton) {
+      //On loading page we hide previous button
+      prevButton.style.visibility = 'hidden';
+    
+      prevButton.addEventListener('click', (e)=> {
+        e.preventDefault();
+  
+        const carousel = e.target.closest('.o-listing--people-carousel').querySelector('.o-listing--people-carousel__items');
+        const itemWidth = carousel.firstElementChild.getBoundingClientRect().width;
+        // this line scrolls the items
+        carousel.scrollLeft -= itemWidth;
+        //visibility next button
+        nextButton.style.visibility = 'visible';
+  
+        let currentItem = currentIndex(carouselContainer, itemWidth) + 1;
+        // Message for screen-reader in the visually-hidden region
+        e.target.closest('.o-listing--people-carousel').querySelector('.liveregion').innerText = ('Item ' + currentItem + ' of ' + allItems.length);
+  
+        const isStartOfScroll = carousel.scrollLeft === 0;
+        prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
+      });
+    }
 
     carouselContainer.addEventListener('scroll', (e)=> {
       const carousel = e.target.closest('.o-listing--people-carousel').querySelector('.o-listing--people-carousel__items');
@@ -105,5 +153,12 @@ Drupal.behaviors.listingPeopleCarousel = {
       prevButton.style.visibility = isStartOfScroll ? 'hidden' : 'visible';
     });
 
+    hideControlButtons(carouselContainer, nextButton, prevButton);
+
+     
+
+    window.addEventListener('resize', () => {
+      hideControlButtons(carouselContainer, nextButton, prevButton);
+    });
   },
 };
