76cf809fc3525761d8d48ad66e44c62a5bb67457
Kate Karpenko <kate@fosterinteractive.com>
2023-08-30 07:19:59 -0600

    fix keyboard accessibility for sliding door menu on mobile

--- web/themes/utmusic/components/02-molecules/sliding-door-menu/sliding-door-menu.js
+++ web/themes/utmusic/components/02-molecules/sliding-door-menu/sliding-door-menu.js
@@ -1,43 +1,183 @@
-Drupal.behaviors.slidingDoorDrupal = {
-  attach(context) {
-    // Find sliding door
-    const slidingDoor = context.querySelector('.m-sliding-door-menu');
-    if (!slidingDoor) {
-      return;
+const resetImageState = (allItemsDoor, itemWidth) => {
+  //set default state 
+  for (let i = 0; i < allItemsDoor.length; i++) {
+    allItemsDoor[i].setAttribute('style', `clip-path: inset(0px ${itemWidth*(allItemsDoor.length - 1 - i)}px 0px ${itemWidth*i}px);`);
+  }
+}
+
+const resetState = (link) => {
+  let highlightedItem = link.closest('.m-sliding-door-menu-wrapper').querySelector('.highlighted-slide');
+  if (highlightedItem) {
+    highlightedItem.classList.remove('highlighted-slide');
+  }
+}
+
+const setHoverState = (allItemsDoor, relatedItem, itemWidth, link) => {
+  let relatedItemId = relatedItem.getAttribute('id');
+  
+  //set clip-path for each item using Id of current link
+  for (let i = 0; i < allItemsDoor.length; i++) {
+    let currentItemId = allItemsDoor[i].getAttribute('id');
+
+    if ((i == 0)&&(currentItemId == relatedItemId)) {
+      allItemsDoor[i].setAttribute('style', `clip-path: inset(0px ${itemWidth*(allItemsDoor.length - 1 - i) - 100}px 0px ${itemWidth*i}px);`);
+    } else if ((i == (allItemsDoor.length - 1)) && (currentItemId == relatedItemId))  {
+      allItemsDoor[i].setAttribute('style', `clip-path: inset(0px ${itemWidth*(allItemsDoor.length - 1 - i)}px 0px ${itemWidth*i - 100}px);`);
+    } else if (currentItemId == relatedItemId) {
+      allItemsDoor[i].setAttribute('style', `clip-path: inset(0px ${itemWidth*(allItemsDoor.length - 1 - i) - 50}px 0px ${itemWidth*i - 50}px);`);
     }
- 
-    const allSlides = slidingDoor.querySelectorAll('.m-sliding-item');
+  }
+  
+  let highlightedItem = link.closest('.m-sliding-door-menu-wrapper').querySelector('.highlighted-slide');
+  if (highlightedItem) {
+    highlightedItem.classList.remove('highlighted-slide');
+  }
+  relatedItem.classList.add('highlighted-slide');
+}
 
-    allSlides.forEach(slide => {
+const linkHoverState = (link, allItemsDoor) => {
+  let previousLink = link.closest('.m-sliding-item__link').previousElementSibling;
+  let nextLink = link.closest('.m-sliding-item__link').nextElementSibling;
 
-      // Listen for mouseenter and focus events
-      slide.addEventListener('mouseenter', () => {
-        let highlightedItem = slide.closest('.m-sliding-door-menu').querySelector('.highlighted-slide');
-        if (highlightedItem) {
-          highlightedItem.classList.remove('highlighted-slide');
-        }
-        slide.classList.add('highlighted-slide');
-      });
+  link.closest('.m-sliding-item__link').setAttribute('style', `flex-basis: calc(${100/allItemsDoor.length}% + 100px)`);
 
-      slide.addEventListener('mouseleave', () => {
-        slide.classList.remove('highlighted-slide');
-      });
-    });
+  if (previousLink && nextLink) {
+    previousLink.setAttribute('style', `flex-basis: calc(${100/allItemsDoor.length}% - 50px)`);
+    nextLink.setAttribute('style', `flex-basis: calc(${100/allItemsDoor.length}% - 50px)`);
+  } else if (previousLink) {
+    previousLink.setAttribute('style', `flex-basis: calc(${100/allItemsDoor.length}% - 100px)`);
+  } else if (nextLink) {
+    nextLink.setAttribute('style', `flex-basis: calc(${100/allItemsDoor.length}% - 100px)`);
+  }
+}
 
-    const slideLinks = slidingDoor.querySelectorAll('.m-sliding-item__link a');
+const linkResetState = (slideLinks, allItemsDoor) => {
+  slideLinks.forEach(link => {
+    link.closest('.m-sliding-item__link').setAttribute('style', `flex-basis: ${100/allItemsDoor.length}%;`);
+  });
+}
+
+const hideLinks = (link) => {
+  let allLinks = link.closest('.m-sliding-door-menu-desktop-links').querySelectorAll('.m-sliding-item__link a');
+
+  allLinks.forEach((hiddenLink) =>{
+    if (link != hiddenLink) {
+      hiddenLink.classList.add('js-hidden');
+    }
+  });
+}
+
+const showLinks = (link) => {
+  let allLinks = link.closest('.m-sliding-door-menu-desktop-links').querySelectorAll('.m-sliding-item__link a');
+
+  allLinks.forEach((hiddenLink) =>{
+    if (link != hiddenLink) {
+      hiddenLink.classList.remove('js-hidden');
+    }
+  });
+}
+
+const mainFunctionality = (slidingDoorWrapper) => {
+  if (window.innerWidth >= 768) {
+    let wrapperWidth = slidingDoorWrapper.offsetWidth;
+    // ratio for image x:y = 117:52
+    slidingDoorWrapper.style.height = (wrapperWidth * 52 / 117) + 'px';
+
+    let allItemsDoor = slidingDoorWrapper.querySelectorAll('.m-sliding-item');
+
+    let itemWidth = wrapperWidth / allItemsDoor.length;
+    resetImageState(allItemsDoor, itemWidth);
+
+    let slideLinks = slidingDoorWrapper.querySelectorAll('.m-sliding-door-menu-desktop-links .m-sliding-item__link a');
 
     slideLinks.forEach(link => {
+      link.closest('.m-sliding-item__link').setAttribute('style', `flex-basis: ${100/allItemsDoor.length}%;`);
+
+      // Get Id of current link and find related item by id
+      const linkId = link.getAttribute('id').replace('link-','');
+      let relatedItem = link.closest('.m-sliding-door-menu-wrapper').querySelector(`#${linkId}`);
+
+      link.addEventListener('mouseenter', () => {
+        hideLinks(link);
+        setHoverState(allItemsDoor, relatedItem, itemWidth, link);
+        linkHoverState(link, allItemsDoor );
+      });
+
+      link.addEventListener('mouseleave', () => {
+        showLinks(link);
+        resetImageState(allItemsDoor, itemWidth);
+        resetState(link);
+        linkResetState(slideLinks, allItemsDoor);
+      });
+
       link.addEventListener('focus', () => {
-        let highlightedItem = link.closest('.m-sliding-door-menu').querySelector('.highlighted-slide');
-        if (highlightedItem) {
-          highlightedItem.classList.remove('highlighted-slide');
-        }
-        link.closest('.m-sliding-item').classList.add('highlighted-slide');
+        hideLinks(link);
+        setHoverState(allItemsDoor, relatedItem, itemWidth, link);
+        linkHoverState(link, allItemsDoor);
       });
 
       link.addEventListener('blur', () => {
-        link.closest('.m-sliding-item').classList.remove('highlighted-slide');
+        showLinks(link);
+        resetImageState(allItemsDoor, itemWidth);
+        resetState(link);
+        linkResetState(slideLinks, allItemsDoor);
       });
+    });
+  }
+}
+
+Drupal.behaviors.slidingDoorDrupal = {
+  attach(context) {
+
+    let slidingDoorWrapper = context.querySelector('.m-sliding-door-menu-wrapper');
+    if (!slidingDoorWrapper) {
+      return;
+    }
+
+    mainFunctionality(slidingDoorWrapper);
+
+    window.addEventListener('resize', ()=> {
+      let slidingDoorWrapper = context.querySelector('.m-sliding-door-menu-wrapper');
+      let allItemsDoor = slidingDoorWrapper.querySelectorAll('.m-sliding-item');
+
+
+      // if mobile or tablet - we should remove all styles which were applied from desktop
+      if (window.innerWidth < 768) {
+        //we don't restrict the height for container
+        slidingDoorWrapper.style.height = 'auto';
+
+        let slideLinks = slidingDoorWrapper.querySelectorAll('.m-sliding-door-menu-desktop-links .m-sliding-item__link a');
+
+
+        slideLinks.forEach(link => {
+          link.closest('.m-sliding-item__link').setAttribute('style', `flex-basis: unset;`);
+    
+          // Get Id of current link and find related item by id
+          const linkId = link.getAttribute('id').replace('link-','');
+          let relatedItem = link.closest('.m-sliding-door-menu-wrapper').querySelector(`#${linkId}`);
+          
+          relatedItem.classList.remove('highlighted-slide');
+
+          // unset clip-path for each item
+          for (let i = 0; i < allItemsDoor.length; i++) {
+            allItemsDoor[i].setAttribute('style', `clip-path: unset`);
+          }
+        });
+
+      } else if (window.innerWidth >= 768) {
+
+        //Calculate the width of container with sliding doors
+        let wrapperWidth = slidingDoorWrapper.offsetWidth;
+        let itemWidth = wrapperWidth / allItemsDoor.length;
+
+        // we should recalculate the height accordinly to ratio
+        slidingDoorWrapper.style.height = (wrapperWidth * 52 / 117) + 'px';
+
+        resetImageState(allItemsDoor, itemWidth);
+      }
+
+      mainFunctionality(slidingDoorWrapper);
+
     });
   },
 };
