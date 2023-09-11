1930871ef5fa13621641b41dc7eae2e32f30467c
Kate Karpenko <kate@fosterinteractive.com>
2023-09-11 08:59:22 -0600

    add image variation for PT:card

--- web/themes/utmusic/components/02-molecules/card/card.js
+++ web/themes/utmusic/components/02-molecules/card/card.js
@@ -1,35 +1,64 @@
 Drupal.behaviors.cardsLinkHoverAnimation = {
   attach(context) {
-    // Selectors
+    // Check if card has a button
     const allCardsLinks = context.querySelectorAll('.m-card--illustration__link a');
 
-    if (!allCardsLinks) {
-      return;
+    if (allCardsLinks) {
+      // On hover add class to card and on leave - remove it 
+      allCardsLinks.forEach((link) => {
+        
+        link.addEventListener('mouseenter', () => {
+          let card = link.closest('.m-card--illustration');
+          card.classList.add('active');
+        });
+
+        link.addEventListener('mouseleave', () => {
+          let card = link.closest('.m-card--illustration');
+          card.classList.remove('active');
+        });
+
+        link.addEventListener('focus', () => {
+          let card = link.closest('.m-card--illustration');
+          card.classList.add('active');
+        });
+
+        link.addEventListener('blur', () => {
+          let card = link.closest('.m-card--illustration');
+          card.classList.remove('active');
+        });
+        
+      });
     }
 
-    // On hover add class to card and on leave - remove it 
-    allCardsLinks.forEach((link) => {
-      
-      link.addEventListener('mouseenter', () => {
-        let card = link.closest('.m-card--illustration');
-        card.classList.add('active');
-      });
 
-      link.addEventListener('mouseleave', () => {
-        let card = link.closest('.m-card--illustration');
-        card.classList.remove('active');
-      });
+    // Check if card has a url on title
+    const allCardsTitlesLinks = context.querySelectorAll('.m-card--illustration .m-card--illustration__title a');
 
-      link.addEventListener('focus', () => {
-        let card = link.closest('.m-card--illustration');
-        card.classList.add('active');
-      });
+    if (allCardsTitlesLinks) {
+      // On hover add class to card and on leave - remove it 
+      allCardsTitlesLinks.forEach((link) => {
+        
+        link.addEventListener('mouseenter', () => {
+          let card = link.closest('.m-card--illustration');
+          card.classList.add('active');
+        });
 
-      link.addEventListener('blur', () => {
-        let card = link.closest('.m-card--illustration');
-        card.classList.remove('active');
+        link.addEventListener('mouseleave', () => {
+          let card = link.closest('.m-card--illustration');
+          card.classList.remove('active');
+        });
+
+        link.addEventListener('focus', () => {
+          let card = link.closest('.m-card--illustration');
+          card.classList.add('active');
+        });
+
+        link.addEventListener('blur', () => {
+          let card = link.closest('.m-card--illustration');
+          card.classList.remove('active');
+        });
+        
       });
-      
-    });
+    }
   },
 };
