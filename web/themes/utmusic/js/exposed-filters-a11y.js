5a5d4e4861ffa2e969f893cea7ad7a64671565a5
Eric homanchuk <erichomanchuk@fosterinteractive.com>
2023-09-18 16:08:45 -0400

    Clean up

--- web/themes/utmusic/js/exposed-filters-a11y.js
+++ web/themes/utmusic/js/exposed-filters-a11y.js
@@ -1,27 +1,23 @@
-(function ($) {
+(function ($, Drupal) {
   'use strict';
 
   Drupal.behaviors.exposedFilters = {
     attach: (context) => {
-
-      $(document).ready(function(){
         
-        const exposedFormButton = $(context).find('.views-exposed-form .form-submit');
-        const offscreenAnnouncement = $(context).find('.offscreen')
+      const exposedFormButton = $(context).find('.views-exposed-form .form-submit');
+      const offscreenAnnouncement = $(context).find('.offscreen')
 
-        // Listen for form submit event
-        exposedFormButton.on('click', function() {
-          // Use Drupal.announce() to announce that the form has been submitted
-          // Drupal.announce( Drupal.t('Filters applied. Results list updated') );
-          Drupal.announce();
+      // Listen for form submit event
+      exposedFormButton.on('click', function() {
+        // Use Drupal.announce() to announce that the form has been submitted
+        // Drupal.announce( Drupal.t('Filters applied. Results list updated') );
+        Drupal.announce();
 
-          // Update off-screen element for screen readers
-          offscreenAnnouncement.text(Drupal.t('Filters applied. Results list updated'));
-          setTimeout(() => offscreenAnnouncement.remove(), 1000);
-
-        });
-
+        // Update off-screen element for screen readers
+        offscreenAnnouncement.text(Drupal.t('Filters applied. Results list updated'));
+        setTimeout(() => offscreenAnnouncement.remove(), 1000);
       });
+
     }
   };
-})(jQuery);
+})(jQuery, Drupal);
